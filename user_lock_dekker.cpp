c17481db301b11a38678b5c90bf7b4f1342d593a
mse-kbk <mse-kbk@DESKTOP-T1494FB>
2025-03-11 14:48:38 +0100

    part2 done

--- user_lock_dekker.cpp
+++ user_lock_dekker.cpp
@@ -1,17 +1,37 @@
 #include "user_locks.hpp"
+#include <atomic>
+
+
+// also works with std::memory_order_acquire
 
 user_lock_dekker::user_lock_dekker()
     : user_lock() {
-    m_flag[0] = m_flag[1] = false;
-    m_turn = false;
+        m_flag[0].store(false, std::memory_order_seq_cst);
+        m_flag[1].store(false, std::memory_order_seq_cst);
+        m_turn.store(0, std::memory_order_seq_cst);
 }
 
 void user_lock_dekker::lock(int thread_id) {
-    // TODO: Implement the lock acquire part of the Dekker algorithm here
-    (void)thread_id; // TODO: Delete this line
+    int other_thread = 1-thread_id;
+
+    m_flag[thread_id].store(true, std::memory_order_seq_cst);
+
+    while (m_flag[other_thread].load(std::memory_order_seq_cst))
+    {
+        if (m_turn.load(std::memory_order_seq_cst) != thread_id) {
+            m_flag[thread_id].store(false, std::memory_order_seq_cst);
+            while (m_turn.load(std::memory_order_seq_cst) != thread_id)  { 
+            } 
+            
+            m_flag[thread_id].store(true, std::memory_order_seq_cst);
+        }
+    } 
 }
 
 void user_lock_dekker::unlock(int thread_id) {
-    // TODO: Implement the lock release part of the Dekker algorithm here
-    (void)thread_id; // TODO: Delete this line
+    int other_thread = 1-thread_id;
+
+    m_turn.store(other_thread, std::memory_order_seq_cst);
+    m_flag[thread_id].store(false, std::memory_order_seq_cst);
+
 }
